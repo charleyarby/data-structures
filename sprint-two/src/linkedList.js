0c36cc8af9e41d0dba13a0d42fa9c809779e057c
charleyarby <charleyarby@gmail.com>
2019-09-06 14:48:04 -0700

    finished linkedlist

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,14 +4,39 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = new Node(value);
+    if (list.head === null) {
+      list.head = node;
+    }
+    else{
+      var current = list.head;
+      while(current.next) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+    list.tail = node;
+    return list.tail;
   };
 
   list.removeHead = function() {
+    var temp = list.head
+    list.head=list.head.next
+    return temp.value;
   };
 
   list.contains = function(target) {
+    var current = list.head;
+    while(current.value !== target && current !== list.tail) {
+      current = current.next;
+    };
+    if (current.value === target) {
+      return true;
+    };
+    else {
+      return false;
+    }
   };
-
   return list;
 };
 
